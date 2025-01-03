84f83d10aba9d5c731e33ea36ce3c2a9e5dc389a
Mohammad Khan <mohammadikhan821@gmail.com>
2025-01-03 18:28:06 -0500

    Add List.py implementation

---
+++ list.py
@@ -0,0 +1,21 @@
+
+# Lists are ordered collectiosn of data just like arrays in other languages
+# Allows different types of elements in the list
+# Similar to ArrayLists and Vectors
+# Costly operation when inserting or deleting elements due to the fact that elements need to be shifted
+
+List = ["Geeks", "For", "Geeks"]
+print("\nList containing multiple values")
+print(List)
+
+List2 = [['Geeks', 'For'], ['Geeks']]
+print("\nMulti-Dimensional List")
+print(List2)
+
+print("Accessing element from the list")
+print(List[0])
+print(List[2])
+
+print("\nAccessing element using negative indexing")
+print(List[-1])
+print(List[-3])
